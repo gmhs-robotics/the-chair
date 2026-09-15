36039433a5bc3d792bb2c31059e119da9371d107
elijah629 <62805599+elijah629@users.noreply.github.com>
2026-09-14 20:03:21 -0700

    fix: rust-version back to vexide-compliant

--- check.sh
+++ check.sh
@@ -5,8 +5,6 @@ cargo fmt --all -- --check
 cargo test --workspace --locked
 cargo clippy --workspace --all-targets --locked -- -D warnings
 cargo audit
-python3 tools/update-target.py
-git diff --exit-code -- .cargo/thumbv7a-vex-v5-chair.json
 ./build.sh
 shellcheck build.sh check.sh render-hud.sh upload.sh
 mdbook build docs
